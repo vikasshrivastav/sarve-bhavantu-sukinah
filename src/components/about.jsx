1ac2072e1c23797a0500299e683c4ed2cbeedd29
Vikas <vikas.virtual@gmail.com>
2020-10-28 11:26:54 +0000

    Pushed updated version

---
+++ src/components/about.jsx
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About Us</span>
+                    <h2 className="colorlib-heading">Who Are We?</h2>
+                    <p>We are </p>
+		    <h2>Sarve bhavantu sukinah organisation</h2>
+		    <p>It's a verse in sanskrit which translates to May everyone be happy.</p>
+		    <p>Our aim is to help the people who are coming from lower strate of society.</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section className="colorlib-about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">What we do?</span>
+                <h2 className="colorlib-heading">Here are some of our work</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Food Distribution </h3>
+                    <p>We are distributing foods to the needy people in Jabalpur(MP) daily.</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Cloth distribution</h3>
+                    <p>This is planned for future</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Education</h3>
+                    <p>This is planned for future</p>
+                </div>
+                </div>
+            </div>
+            {/*
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-2">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Education</h3>
+                    <p>This is planned for future </p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-4">
+                <span className="icon">
+                    <i className="icon-layers2" />
+                </span>
+                <div className="desc">
+                    <h3>Graphic Design</h3>
+                    <p>My friend knows .. P</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-6">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Digital Marketing</h3>
+                    <p>I use Instagram eight hours a day :) </p>
+                </div>
+                </div>
+            </div>
+            */}
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/components/introduction.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+
+export default class Introduction extends Component {
+  render() {
+    return (
+      <div>
+        <section id="colorlib-hero" className="js-fullheight" data-section="home">
+          <div className="flexslider js-fullheight">
+            <ul className="slides">
+              <li style={{backgroundImage: 'url(images/img-02.jpg)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1>Hi! <br />We are Sarve Bhavantu Sukinah Organisation</h1>
+                          <p><a className="btn btn-primary btn-learn" href="" target="_blank" rel="noopener noreferrer">Donate<i className="icon-download4" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{backgroundImage: 'url(images/img-02.jpg)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h3>It's a verse in Sanskrit which translates to..</h3>
+			  <h1>May everyone be happy </h1>
+                          <p><a className="btn btn-primary btn-learn" href="" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{backgroundImage: 'url(images/img-02.jpg)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h2>Our aim is to help the people who are coming from lower strata of society </h2>
+                          <p><a className="btn btn-primary btn-learn" href="" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
